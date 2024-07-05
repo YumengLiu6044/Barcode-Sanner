11f6d592b5e5fda87b8ed4854c3c68fe32bf61e2
Yumeng Liu <lym20041026@gmail.com>
2024-07-05 10:04:51 -0700

    Initial Commit

---
+++ Barcode Sanner/Barcode_SannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_SannerApp.swift
+//  Barcode Sanner
+//
+//  Created by Yumeng Liu on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_SannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
